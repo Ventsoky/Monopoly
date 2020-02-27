df326f4ef4723e8db409f6c85e4f7cd1fd1c89d0
Vencislav Borislavov <vkosevb@gmail.com>
2020-02-27 18:41:55 +0200

    Main now is game logic + the begining of the end

--- Game.java
+++ Game.java
@@ -1,6 +1,28 @@
 package Vencislav;
 public class Game{
     public static void main(String[] args) {
+        GameLogic game = new GameLogic();
+        game.gameSetUp();
+        while (true){
+            if(game.prison()){
+                continue;
+            }
 
+            game.playerSetUp();
+            if(game.jailBlock()){
+                continue;
+            }
+
+            game.normalPlace();
+
+            game.chanceOrCommunityChest();
+
+            game.charging();
+
+            game.menu();
+
+            game.nextPlayer();
+
+        }
     }
 }
